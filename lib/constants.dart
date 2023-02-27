5efcb3c3a39a3bd9441fb055404d205b0ed44425
Ayodele Fagbami <afagbami@gmail.com>
2023-02-27 12:49:05 +0100

    added color constants

---
+++ lib/constants.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+const kBgColor = Color(0xFFFFFFFF);
+const kPrimaryColor = Color(0xFF273A69);
+const kCallToAction = Color(0xFFBCC7FF);
+const kBlacks = Color(0xFF000000);
+const kGreys = Color(0xFF6B7280);
+const kLightGreys = Color(0xFFF9FAFB);
+const kLight30 = Color(0xFF5F8DF7);
+const kOutlineVariant = Color(0xFFC4C7C5);
