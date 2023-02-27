a72eb384b3d9b0a8316f37576aa99eb357764f7a
Ayodele Fagbami <afagbami@gmail.com>
2023-02-27 21:20:11 +0100

    moved onboarding content to widgets

--- lib/features/onboarding/presentation/widgets/body.dart
+++ lib/features/onboarding/presentation/widgets/body.dart
@@ -4,6 +4,7 @@ import 'package:flutter_svg/svg.dart';
 import '../../../../components/default_button.dart';
 import '../../../../constants.dart';
 import '../../../../size_config.dart';
+import 'onboarding_content.dart';
 
 class Body extends StatefulWidget {
   const Body({super.key});
@@ -114,92 +115,5 @@ class _BodyState extends State<Body> {
               shape: BoxShape.circle,
             ),
           );
-  }
-}
-
-class OnboardingContent extends StatelessWidget {
-  const OnboardingContent({
-    Key? key,
-    required this.text,
-    required this.index,
-    required this.image,
-    required this.header,
-  }) : super(key: key);
-  final String? image, header, text;
-  final int index;
-
-  @override
-  Widget build(BuildContext context) {
-    return Stack(
-      children: <Widget>[
-        SizedBox(
-          width: double.infinity,
-          child: Image.asset(
-            image!,
-            height: getProportionateScreenHeight(407.26),
-            width: getProportionateScreenWidth(202.68),
-          ),
-        ),
-        Padding(
-          padding: EdgeInsets.only(top: getProportionateScreenHeight(288)),
-          child: Container(
-            decoration: BoxDecoration(
-              color: kBgColor,
-              boxShadow: [
-                BoxShadow(
-                  color: kBgColor.withOpacity(0.91),
-                  blurRadius: 25,
-                  offset: const Offset(0, -50),
-                ),
-              ],
-            ),
-            child: Center(
-              child: Column(
-                children: [
-                  Column(
-                    children: <Widget>[
-                      Padding(
-                        padding: EdgeInsets.symmetric(
-                          horizontal: getProportionateScreenWidth(16),
-                          vertical: getProportionateScreenHeight(8),
-                        ),
-                        child: Text(
-                          header!,
-                          style: Theme.of(context)
-                              .textTheme
-                              .displayMedium!
-                              .copyWith(
-                                color: kPrimaryColor,
-                              ),
-                          textAlign: TextAlign.center,
-                        ),
-                      ),
-                      SizedBox(
-                        height: getProportionateScreenHeight(8),
-                      ),
-                      Padding(
-                        padding: EdgeInsets.symmetric(
-                          horizontal: getProportionateScreenWidth(8),
-                          vertical: getProportionateScreenHeight(8),
-                        ),
-                        child: Text(
-                          text!,
-                          style: Theme.of(context)
-                              .textTheme
-                              .bodySmall!
-                              .copyWith(color: kPrimaryColor),
-                          textAlign: TextAlign.center,
-                        ),
-                      ),
-                    ],
-                  ),
-                  // SizedBox(height: getProportionateScreenHeight(32)),
-                ],
-              ),
-            ),
-          ),
-        ),
-      ],
-    );
   }
 }
