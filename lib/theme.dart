1ee6e3b74f2837310643f189292c9013cbf338e6
Ayodele Fagbami <afagbami@gmail.com>
2023-02-27 14:35:52 +0100

    added text and appbar themes

--- lib/theme.dart
+++ lib/theme.dart
@@ -1,7 +1,61 @@
 import 'package:flutter/material.dart';
+import 'constants.dart';
+import 'size_config.dart';
 
 ThemeData theme() {
   return ThemeData(
-    primarySwatch: Colors.blue,
+    scaffoldBackgroundColor: kBgColor,
+    appBarTheme: appBarTheme(),
+    visualDensity: VisualDensity.adaptivePlatformDensity,
+    textTheme: textTheme(),
+  );
+}
+
+TextTheme textTheme() {
+  return const TextTheme(
+    displayLarge: TextStyle(
+      fontSize: 28,
+      fontWeight: FontWeight.bold,
+      letterSpacing: -0.4,
+      color: kBlacks,
+    ),
+    displayMedium: TextStyle(
+      fontSize: 24,
+      fontWeight: FontWeight.bold,
+      letterSpacing: -0.4,
+      color: kBlacks,
+    ),
+    displaySmall: TextStyle(
+      fontSize: 20,
+      fontWeight: FontWeight.bold,
+      letterSpacing: 0,
+      color: kBlacks,
+    ),
+    bodyLarge: TextStyle(
+      fontSize: 16,
+      fontWeight: FontWeight.normal,
+      letterSpacing: 0,
+      color: kBlacks,
+    ),
+    bodyMedium: TextStyle(
+      fontSize: 14,
+      fontWeight: FontWeight.normal,
+      letterSpacing: 0,
+      color: kBlacks,
+    ),
+    bodySmall: TextStyle(
+      fontSize: 12,
+      fontWeight: FontWeight.normal,
+      letterSpacing: 0,
+      color: kBlacks,
+    ),
+  );
+}
+
+AppBarTheme appBarTheme() {
+  return const AppBarTheme(
+    centerTitle: true,
+    elevation: 0,
+    color: kBgColor,
   );
 }
