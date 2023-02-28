001c7b7330c33830ff63911cae4a01c3dbcdbcbd
Ayodele Fagbami <afagbami@gmail.com>
2023-02-28 20:39:04 +0100

    phone number screen UI

--- lib/features/auth/presentation/pages/phone_number_screen.dart
+++ lib/features/auth/presentation/pages/phone_number_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:servisgo/components/default_button.dart';
+import 'package:servisgo/constants.dart';
 import '../widgets/form_header.dart';
 
 import '../../../../size_config.dart';
@@ -9,15 +11,47 @@ class PhoneNumberScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        children: <Widget>[
-          SizedBox(height: getProportionateScreenHeight(106)),
-          const FormHeader(
-            title: "Phone Number",
-            subtitle:
-                "Please Enter your phone number so we can send you a code and verify",
-          ),
-        ],
+      body: Padding(
+        padding:
+            EdgeInsets.symmetric(horizontal: getProportionateScreenWidth(32)),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            SizedBox(height: getProportionateScreenHeight(106)),
+            const FormHeader(
+              title: "Phone Number",
+              subtitle:
+                  "Please Enter your phone number so we can send you a code and verify",
+            ),
+            SizedBox(height: getProportionateScreenHeight(40)),
+            Form(
+              child: Column(
+                children: <Widget>[
+                  TextFormField(
+                    style: Theme.of(context).textTheme.bodyLarge!.copyWith(
+                          fontWeight: FontWeight.w600,
+                          color: kPrimaryColor,
+                        ),
+                    decoration: InputDecoration(
+                      hintText: "Phone Number",
+                      hintStyle: TextStyle(
+                        fontSize: getProportionateScreenWidth(16),
+                        fontWeight: FontWeight.normal,
+                        letterSpacing: 0,
+                        color: kGreys,
+                      ),
+                    ),
+                  ),
+                  SizedBox(height: getProportionateScreenHeight(132)),
+                  DefaultButton(
+                    text: "Send Code",
+                    press: () {},
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
