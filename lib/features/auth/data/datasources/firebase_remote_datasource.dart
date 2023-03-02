7315bef6c14bd9605f7e75de390c76875ec49cf9
Ayodele Fagbami <afagbami@gmail.com>
2023-03-02 15:25:32 +0100

    Created firebase remote datasource

---
+++ lib/features/auth/data/datasources/firebase_remote_datasource.dart
@@ -0,0 +1,13 @@
+abstract class FirebaseRemoteDatasource {
+  Future<void> signUp(String email, String password);
+  Future<void> signIn(String email, String password);
+  Future<bool> isSignIn();
+  Future<String> getCurrentUid();
+  Future<void> createCurrentUser(
+    String uid,
+    String name,
+    String email,
+    String phoneNumber,
+    String address,
+  );
+}
