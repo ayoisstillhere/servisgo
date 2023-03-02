6900d8fa747486520201503e39bf6a327cd189bc
Ayodele Fagbami <afagbami@gmail.com>
2023-03-02 15:46:51 +0100

    added create user to firebase repo

--- lib/features/auth/domain/repositories/firebase_repository.dart
+++ lib/features/auth/domain/repositories/firebase_repository.dart
@@ -3,4 +3,12 @@ abstract class FirebaseRepository {
   Future<void> signIn(String email, String password);
   Future<bool> isSignIn();
   Future<String> getCurrentUid();
+  Future<void> createCurrentUser(
+    String uid,
+    String name,
+    String email,
+    String phoneNumber,
+    String address,
+    String pfpURL,
+  );
 }
