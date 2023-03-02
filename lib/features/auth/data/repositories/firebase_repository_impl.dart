a759c83bb04e7fae3152264b0b7c63c7f014c430
Ayodele Fagbami <afagbami@gmail.com>
2023-03-02 14:51:44 +0100

    Created firebase repo impl

---
+++ lib/features/auth/data/repositories/firebase_repository_impl.dart
@@ -0,0 +1,9 @@
+import 'package:servisgo/features/auth/domain/repositories/firebase_repository.dart';
+
+class FirebaseRepositoryImpl implements FirebaseRepository {
+  @override
+  Future<void> signUp(String email, String password) {
+    // TODO: implement signUp
+    throw UnimplementedError();
+  }
+}
