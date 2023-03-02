170932c7ea1eb8d2f700225c4316223e88f78a6e
Ayodele Fagbami <afagbami@gmail.com>
2023-03-02 18:33:12 +0100

    state mgmt for sign in and registeration

--- lib/features/auth/presentation/bloc/signin_cubit/signin_cubit.dart
+++ lib/features/auth/presentation/bloc/signin_cubit/signin_cubit.dart
@@ -1,8 +1,51 @@
+import 'dart:io';
+
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
+
+import 'package:servisgo/features/auth/domain/usecases/create_current_user_usecase.dart';
+import 'package:servisgo/features/auth/domain/usecases/signin_usecase.dart';
+import 'package:servisgo/features/auth/domain/usecases/signup_usecase.dart';
 
 part 'signin_state.dart';
 
 class SigninCubit extends Cubit<SigninState> {
-  SigninCubit() : super(SigninInitial());
+  final SignupUsecase signupUsecase;
+  final SigninUsecase signinUsecase;
+  final CreateCurrentUserUsecase createCurrentUserUsecase;
+  SigninCubit(
+    this.signupUsecase,
+    this.signinUsecase,
+    this.createCurrentUserUsecase,
+  ) : super(SigninInitial());
+
+  Future<void> submitSignin({
+    required String email,
+    required String password,
+  }) async {
+    emit(SigninLoading());
+    try {
+      signinUsecase.call(email, password);
+      emit(SigninSuccess());
+    } on SocketException catch (e) {
+      emit(SigninFailure(e.message));
+    } catch (_) {
+      emit(const SigninFailure("Firebase Exception"));
+    }
+  }
+
+  Future<void> submitRegisteration({
+    required String email,
+    required String password,
+  }) async {
+    emit(SigninLoading());
+    try {
+      signupUsecase.call(email, password);
+      emit(SigninSuccess());
+    } on SocketException catch (e) {
+      emit(SigninFailure(e.message));
+    } catch (_) {
+      emit(const SigninFailure("Firebase Exception"));
+    }
+  }
 }
--- lib/features/auth/presentation/bloc/signin_cubit/signin_state.dart
+++ lib/features/auth/presentation/bloc/signin_cubit/signin_state.dart
@@ -2,9 +2,27 @@ part of 'signin_cubit.dart';
 
 abstract class SigninState extends Equatable {
   const SigninState();
-
-  @override
-  List<Object> get props => [];
 }
 
-class SigninInitial extends SigninState {}
+class SigninInitial extends SigninState {
+  @override
+  List<Object?> get props => [];
+}
+
+class SigninLoading extends SigninState {
+  @override
+  List<Object?> get props => [];
+}
+
+class SigninFailure extends SigninState {
+  final String errorMessage;
+
+  const SigninFailure(this.errorMessage);
+  @override
+  List<Object?> get props => [errorMessage];
+}
+
+class SigninSuccess extends SigninState {
+  @override
+  List<Object?> get props => [];
+}
