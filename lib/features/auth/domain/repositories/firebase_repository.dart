d8838a3e168349517f98b9faea17173cec654c2b
Ayodele Fagbami <afagbami@gmail.com>
2023-03-02 14:59:35 +0100

    added more methods to firebase repo

--- lib/features/auth/domain/repositories/firebase_repository.dart
+++ lib/features/auth/domain/repositories/firebase_repository.dart
@@ -1,3 +1,6 @@
 abstract class FirebaseRepository {
   Future<void> signUp(String email, String password);
+  Future<void> signIn(String email, String password);
+  Future<bool> isSignIn();
+  Future<String> getCurrentUid();
 }
