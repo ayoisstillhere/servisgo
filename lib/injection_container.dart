849f4720047f59cd6a4e993c467682fc9166f789
Ayodele Fagbami <afagbami@gmail.com>
2023-03-02 17:44:32 +0100

    Created injection Container

---
+++ lib/injection_container.dart
@@ -0,0 +1,16 @@
+import 'package:get_it/get_it.dart';
+
+final sl = GetIt.instance;
+
+Future<void> init() async {
+  //Features bloc,
+
+  //!useCae
+
+  //repository
+
+  //datasource
+
+  //external
+  //e.g final shared Preference = await SharedPreferences.getInstance();
+}
