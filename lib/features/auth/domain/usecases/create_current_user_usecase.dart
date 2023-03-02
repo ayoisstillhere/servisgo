b7273d31a05b25ebcb04e3c7b7bd8cd9999591de
Ayodele Fagbami <afagbami@gmail.com>
2023-03-02 15:53:21 +0100

    create current user usecase

---
+++ lib/features/auth/domain/usecases/create_current_user_usecase.dart
@@ -0,0 +1,25 @@
+import 'package:servisgo/features/auth/domain/repositories/firebase_repository.dart';
+
+class CreateCurrentUserUsecase {
+  final FirebaseRepository repository;
+
+  CreateCurrentUserUsecase(this.repository);
+
+  Future<void> call(
+    String uid,
+    String name,
+    String email,
+    String phoneNumber,
+    String address,
+    String pfpURL,
+  ) async {
+    repository.createCurrentUser(
+      uid,
+      name,
+      email,
+      phoneNumber,
+      address,
+      pfpURL,
+    );
+  }
+}
