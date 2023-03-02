8c123e2ea055666e59eaacd6d076cbbb6a14da51
Ayodele Fagbami <afagbami@gmail.com>
2023-03-02 13:53:28 +0100

    Firebase ios setup

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
 
 import 'features/onboarding/presentation/pages/onboarding_screen.dart';
 import 'theme.dart';
 
-void main() {
+Future main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp();
+
   runApp(const MyApp());
 }
 
