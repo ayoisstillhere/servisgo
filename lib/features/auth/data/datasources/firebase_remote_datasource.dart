6fa8349d1e8075341f9e7bf66dbe86f72dbf2d48
Ayodele Fagbami <afagbami@gmail.com>
2023-03-02 15:44:44 +0100

    impl of remote datasource

--- lib/features/auth/data/datasources/firebase_remote_datasource.dart
+++ lib/features/auth/data/datasources/firebase_remote_datasource.dart
@@ -1,3 +1,7 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:servisgo/features/auth/data/models/user_model.dart';
+
 abstract class FirebaseRemoteDatasource {
   Future<void> signUp(String email, String password);
   Future<void> signIn(String email, String password);
@@ -11,4 +15,57 @@ abstract class FirebaseRemoteDatasource {
     String address,
     String pfpURL,
   );
+}
+
+class FirebaseRemoteDataSourceImpl implements FirebaseRemoteDatasource {
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+  final _userCollection = FirebaseFirestore.instance.collection("users");
+
+  @override
+  Future<void> createCurrentUser(
+    String uid,
+    String name,
+    String email,
+    String phoneNumber,
+    String address,
+    String pfpURL,
+  ) async {
+    _userCollection.doc(_auth.currentUser!.uid).get().then((user) {
+      if (!user.exists) {
+        final newUser = UserModel(
+          uid: uid,
+          name: name,
+          email: email,
+          phoneNumber: phoneNumber,
+          address: address,
+          pfpURL: pfpURL,
+        ).toDocument();
+        _userCollection.doc(_auth.currentUser!.uid).set(newUser);
+        return;
+      }
+    });
+  }
+
+  @override
+  Future<String> getCurrentUid() async => _auth.currentUser!.uid;
+
+  @override
+  // ignore: unnecessary_null_comparison
+  Future<bool> isSignIn() async => _auth.currentUser!.uid != null;
+
+  @override
+  Future<void> signIn(String email, String password) async {
+    await _auth.signInWithEmailAndPassword(
+      email: email,
+      password: password,
+    );
+  }
+
+  @override
+  Future<void> signUp(String email, String password) async {
+    await _auth.createUserWithEmailAndPassword(
+      email: email,
+      password: password,
+    );
+  }
 }
