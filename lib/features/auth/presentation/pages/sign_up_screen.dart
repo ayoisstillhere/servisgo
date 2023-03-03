c4feabd07e868af0b4b2347807b19436dc7cdfaa
Ayodele Fagbami <afagbami@gmail.com>
2023-03-03 11:39:03 +0100

    added submit reg to sign up

--- lib/features/auth/presentation/pages/sign_up_screen.dart
+++ lib/features/auth/presentation/pages/sign_up_screen.dart
@@ -1,9 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_svg/svg.dart';
+import 'package:servisgo/features/auth/presentation/bloc/signin_cubit/signin_cubit.dart';
 
 import '../../../../components/default_button.dart';
 import '../../../../constants.dart';
 import '../../../../size_config.dart';
+import '../../../home/presentation/pages/home_screen.dart';
 import '../widgets/form_error.dart';
 import 'phone_number_screen.dart';
 import 'sign_in_screen.dart';
@@ -58,116 +61,127 @@ class _SignUpScreenState extends State<SignUpScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Padding(
-        padding:
-            EdgeInsets.symmetric(horizontal: getProportionateScreenWidth(32)),
-        child: SingleChildScrollView(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: <Widget>[
-              SizedBox(height: getProportionateScreenHeight(106)),
-              RichText(
-                text: TextSpan(
-                  text: "Create a Servis",
-                  style: Theme.of(context).textTheme.displayMedium!.copyWith(
-                        color: kPrimaryColor,
-                      ),
-                  children: <TextSpan>[
-                    TextSpan(
-                      text: "Go\n",
-                      style: Theme.of(context)
-                          .textTheme
-                          .displayMedium!
-                          .copyWith(color: kCallToAction),
-                    ),
-                    TextSpan(
-                      text: "account",
-                      style: Theme.of(context)
-                          .textTheme
-                          .displayMedium!
-                          .copyWith(color: kPrimaryColor),
-                    ),
-                  ],
-                ),
-              ),
-              SizedBox(height: getProportionateScreenHeight(52)),
-              Form(
-                key: _signupFormKey,
-                child: Column(
-                  children: <Widget>[
-                    _buildNameTextFormField(context),
-                    SizedBox(height: getProportionateScreenHeight(24)),
-                    _buildEmailTextFormField(context),
-                    SizedBox(height: getProportionateScreenHeight(24)),
-                    _buildPasswordTextformField(context),
-                    SizedBox(height: getProportionateScreenHeight(40)),
-                    FormError(errors: errors),
-                    DefaultButton(
-                      text: "Sign Up",
-                      press: () {
-                        Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                                builder: (context) =>
-                                    const PhoneNumberScreen()));
-                      },
-                    ),
-                    SizedBox(height: getProportionateScreenHeight(40)),
-                    Center(child: SvgPicture.asset("assets/images/or.svg")),
-                    SizedBox(height: getProportionateScreenHeight(30)),
-                    Container(
-                      height: getProportionateScreenHeight(56),
-                      decoration: BoxDecoration(
-                        border: Border.all(color: const Color(0xFFE5E7EB)),
-                        borderRadius: BorderRadius.circular(
-                            getProportionateScreenWidth(20)),
-                      ),
-                      child: Center(
-                        child: SvgPicture.asset(
-                          "assets/icons/google_icon.svg",
+      body: BlocConsumer<SigninCubit, SigninState>(
+        listener: (context, state) {
+          if (state is SigninSuccess) {
+            Navigator.push(context,
+                MaterialPageRoute(builder: (_) => const PhoneNumberScreen()));
+          }
+          if (state is SigninFailure) {
+            addError(error: "Invalid Login");
+          }
+        },
+        builder: (context, state) {
+          if (state is SigninLoading) {
+            return const Center(child: CircularProgressIndicator());
+          }
+          return Padding(
+            padding: EdgeInsets.symmetric(
+                horizontal: getProportionateScreenWidth(32)),
+            child: SingleChildScrollView(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  SizedBox(height: getProportionateScreenHeight(106)),
+                  RichText(
+                    text: TextSpan(
+                      text: "Create a Servis",
+                      style:
+                          Theme.of(context).textTheme.displayMedium!.copyWith(
+                                color: kPrimaryColor,
+                              ),
+                      children: <TextSpan>[
+                        TextSpan(
+                          text: "Go\n",
+                          style: Theme.of(context)
+                              .textTheme
+                              .displayMedium!
+                              .copyWith(color: kCallToAction),
                         ),
-                      ),
+                        TextSpan(
+                          text: "account",
+                          style: Theme.of(context)
+                              .textTheme
+                              .displayMedium!
+                              .copyWith(color: kPrimaryColor),
+                        ),
+                      ],
                     ),
-                    SizedBox(height: getProportionateScreenHeight(88)),
-                    Center(
-                      child: GestureDetector(
-                        onTap: () {
-                          Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                              builder: (context) => const SignInScreen(),
+                  ),
+                  SizedBox(height: getProportionateScreenHeight(52)),
+                  Form(
+                    key: _signupFormKey,
+                    child: Column(
+                      children: <Widget>[
+                        _buildNameTextFormField(context),
+                        SizedBox(height: getProportionateScreenHeight(24)),
+                        _buildEmailTextFormField(context),
+                        SizedBox(height: getProportionateScreenHeight(24)),
+                        _buildPasswordTextformField(context),
+                        SizedBox(height: getProportionateScreenHeight(40)),
+                        FormError(errors: errors),
+                        DefaultButton(
+                          text: "Sign Up",
+                          press: _submitRegisteration,
+                        ),
+                        SizedBox(height: getProportionateScreenHeight(40)),
+                        Center(child: SvgPicture.asset("assets/images/or.svg")),
+                        SizedBox(height: getProportionateScreenHeight(30)),
+                        Container(
+                          height: getProportionateScreenHeight(56),
+                          decoration: BoxDecoration(
+                            border: Border.all(color: const Color(0xFFE5E7EB)),
+                            borderRadius: BorderRadius.circular(
+                                getProportionateScreenWidth(20)),
+                          ),
+                          child: Center(
+                            child: SvgPicture.asset(
+                              "assets/icons/google_icon.svg",
                             ),
-                          );
-                        },
-                        child: RichText(
-                          text: TextSpan(
-                              text: "Already have an account? ",
-                              style: Theme.of(context)
-                                  .textTheme
-                                  .bodyLarge!
-                                  .copyWith(
-                                    color: kGreys,
-                                  ),
-                              children: <TextSpan>[
-                                TextSpan(
-                                  text: 'Sign In',
+                          ),
+                        ),
+                        SizedBox(height: getProportionateScreenHeight(88)),
+                        Center(
+                          child: GestureDetector(
+                            onTap: () {
+                              Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                  builder: (context) => const SignInScreen(),
+                                ),
+                              );
+                            },
+                            child: RichText(
+                              text: TextSpan(
+                                  text: "Already have an account? ",
                                   style: Theme.of(context)
                                       .textTheme
                                       .bodyLarge!
                                       .copyWith(
-                                        color: kCallToAction,
+                                        color: kGreys,
                                       ),
-                                ),
-                              ]),
+                                  children: <TextSpan>[
+                                    TextSpan(
+                                      text: 'Sign In',
+                                      style: Theme.of(context)
+                                          .textTheme
+                                          .bodyLarge!
+                                          .copyWith(
+                                            color: kCallToAction,
+                                          ),
+                                    ),
+                                  ]),
+                            ),
+                          ),
                         ),
-                      ),
+                      ],
                     ),
-                  ],
-                ),
+                  ),
+                ],
               ),
-            ],
-          ),
-        ),
+            ),
+          );
+        },
       ),
     );
   }
@@ -268,12 +282,12 @@ class _SignUpScreenState extends State<SignUpScreen> {
         }
       },
       validator: (value) {
-            if (value!.isEmpty) {
-              addError(error: kNameNullError);
-              return "";
-            }
-            return null;
-          },
+        if (value!.isEmpty) {
+          addError(error: kNameNullError);
+          return "";
+        }
+        return null;
+      },
       controller: _nameController,
       style: Theme.of(context).textTheme.bodyLarge!.copyWith(
             fontWeight: FontWeight.w600,
@@ -289,5 +303,19 @@ class _SignUpScreenState extends State<SignUpScreen> {
         ),
       ),
     );
+  }
+
+  void _submitRegisteration() async {
+    if (_signupFormKey.currentState!.validate()) {
+      _signupFormKey.currentState!.save();
+      await BlocProvider.of<SigninCubit>(context).submitRegisteration(
+        email: _emailController.text.trim(),
+        password: _passwordController.text.trim(),
+        name: _nameController.text.trim(),
+        phoneNumber: "",
+        address: "",
+        pfpURL: "",
+      );
+    }
   }
 }
